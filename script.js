96a86151ba1a602c0c15ec9b567cea14ff251155
vishwasshar <88779312+vishwasshar@users.noreply.github.com>
2022-03-17 09:49:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#submit").addEventListener("click", function () {
-    let contentArr = document.querySelector("#text").value.split('\n');
-    let contentSet = new Set(contentArr);
-    let contentSetArr = []
-    for (const i of contentSet.values()) {
-        contentSetArr.push(i);
-    }
-    document.querySelector('#modText').value = contentSetArr.join('\n');
-});
