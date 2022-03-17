9a48a3e0fe8cd667a2112dc5e43c2ebe2affa4ef
vishwasshar <88779312+vishwasshar@users.noreply.github.com>
2022-03-17 12:41:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.querySelector("#submit").addEventListener("click", function () {
-    let contentArr = document.querySelector("#text").value.split('\n');
-    let contentSet = new Set(contentArr);
-    let contentSetArr = []
-    for (const i of contentSet.values()) {
-        contentSetArr.push(i);
-    }
-    document.querySelector('#modText').value = contentSetArr.join('\n');
+  let contentArr = document.querySelector("#text").value.split("\n");
+  let contentSet = new Set(contentArr);
+  let contentSetArr = [];
+  for (const i of contentSet.values()) {
+    contentSetArr.push(i);
+  }
+  document.querySelector("#modText").value = contentSetArr.join("\n");
 });
